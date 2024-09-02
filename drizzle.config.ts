7f88992691f9ff9dff5ffffa120aa0a44000624d
Kristin <kboeckma@stumail.jccc.edu>
2024-09-02 13:05:41 -0500

    Adjustments to not expose database url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://Summa_owner:9NZFnpfq3VYr@ep-damp-frost-a5xvw2l4.us-east-2.aws.neon.tech/Summa?sslmode=require',
+      url:process.env.NEXT_PUBLIC_DATABASE_URL,
     }
   };
--- utils/dbConfig.tsx
+++ utils/dbConfig.tsx
@@ -2,9 +2,9 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.NEXT_PUBLIC_DATABASE_URL) {
     throw new Error("DATABASE_URL is not set in the environment variables.");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 const db = drizzle(sql,{schema});
