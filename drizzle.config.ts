eee81d9d81c88a63ed6c72cbbadcb70eccd042bc
Kristin <kboeckma@ku.edu>
2025-03-15 12:56:17 -0500

    Fixed code to only categories created by the user only

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
-/** @type { import("drizzle-kit").Config } */
-const config = {
-  schema: "./utils/schema.tsx",
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './utils/schema.tsx',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DATABASE_URL || 'postgresql://Summa_owner:9NZFnpfq3VYr@ep-damp-frost-a5xvw2l4.us-east-2.aws.neon.tech/Summa?sslmode=require',
-  }
-};
-
-export default config;
+    url: process.env.DATABASE_URL!,
+  },
+});
